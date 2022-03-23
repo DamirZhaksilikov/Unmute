76b94b648052f8a097b2a4eac52c032be5189cb9
Damir <damir.zhaksilikov@gmail.com>
2022-03-23 11:35:26 +0100

    Added Elis' changes

--- src/CategoriesPage.js
+++ src/CategoriesPage.js
@@ -2,10 +2,10 @@ import React from 'react';
 import './CategoriesPage.css'
 
 const restaurantData = [
-    { imageSource: "ice-cream.png", audioSource: null, scoreCount: 0, phrase: ["This is phrase 1", "This is phrase 2", "This is phrase 3"] },
-    { imageSource: "burger.png", audioSource: null, scoreCount: 0, phrase: ["This is phrase 1", "This is phrase 2", "This is phrase 3"] },
-    { imageSource: "restroom.png", audioSource: null, scoreCount: 0, phrase: ["This is phrase 1", "This is phrase 2", "This is phrase 3"] },
-];
+    { imageSource: "ice-cream.png", audioSource: null, scoreCount: 0, phrase: ["Ice Cream", "I would like to have an ice cream.", "Can I have an ice cream, please?"] },
+    { imageSource: "burger.png", audioSource: null, scoreCount: 0, phrase: ["Burger", "I want a hamburger. ", "Can I have a hamburguer, please?"] },
+    { imageSource: "restroom.png", audioSource: null, scoreCount: 0, phrase: ["Toilet", "I want to use the toilet.", "May I use the toilet, please?"] }
+]
 
 const schoolData = [];
 const partyData = [];
@@ -28,17 +28,17 @@ class CategoriesPage extends React.Component {
                     <div className='category-caption'>{"Restaurant"}</div>
                 </div>
 
-                <div className='category'>
+                <div className='category disabled'>
                     <img className='category-image' src='School-category.png' />
                     <div className='category-caption'>{"School"}</div>
                 </div>
 
-                <div className='category'>
+                <div className='category disabled'>
                     <img className='category-image' src='Party-category.png' />
                     <div className='category-caption'>{"Party"}</div>
                 </div>
 
-                <div className='category'>
+                <div className='category disabled'>
                     <img className='category-image' src='Transport-category.png' />
                     <div className='category-caption'>{"Transport"}</div>
                 </div>
